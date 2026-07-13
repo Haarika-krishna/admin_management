3249e0487cf6e2ef0bff85d163d9dc32df38f21d
Haarika-krishna <boiniharika2109@gmail.com>
2026-07-13 14:13:00 +0530

    Main chnaged

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,13 @@
 import { StrictMode } from "react";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { Routes, Route } from "react-router-dom";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/admin_management">
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
